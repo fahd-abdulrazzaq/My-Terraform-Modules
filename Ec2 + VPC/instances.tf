19b71a65def262d988869e17c96f0a5959a114a1
Fahd Abdulrazzaq <fadabdul15@gmail.com>
2023-05-17 01:15:19 +0100

    Update instances.tf

--- Ec2 + VPC/instances.tf
+++ Ec2 + VPC/instances.tf
@@ -1,1 +1,14 @@
+resource "aws_instance" "test-svr" {
+  ami                    = var.AMIS
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.testsub-pub.id
+  vpc_security_group_ids = [aws_security_group.test-SG.id]
+  tags = {
+    Name = "test"
+  }
+}
 
+
+output "PublicIP" {
+  value = aws_instance.test-svr.public_ip
+}
