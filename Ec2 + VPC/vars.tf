96b7fe02c45e3c843931efcf2490ac59ddbd27ac
Fahd Abdulrazzaq <fadabdul15@gmail.com>
2023-05-17 01:16:12 +0100

    Create vars.tf

---
+++ Ec2 + VPC/vars.tf
@@ -0,0 +1,30 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+
+variable "ZONE2" {
+  default = "us-east-1b"
+}
+variable "ZONE3" {
+  default = "us-east-1c"
+}
+
+variable "AMIS" {
+  default = "ami-007855ac798b5175e"
+}
+
+variable "USER" {
+  default = "ubuntu"
+}
+
+variable "MYIP" {
+  default = "102.91.55.79/32"
+}
+
+variable "AWSKEY" {
+  default = "vagrantkey-1.pub"
+}
