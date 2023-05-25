561aefb701178400ed95a422821d5ed5628255e3
Fahd Abdulrazzaq <fadabdul15@gmail.com>
2023-05-25 22:50:36 +0100

    Create vars.tf
    initial commit

---
+++ Vpc & Subnets/vars.tf
@@ -0,0 +1,18 @@
+
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+
+
+variable "ZONE2" {
+  default = "us-east-1b"
+}
+
+variable "AMIS" {
+
+  default = "ami-053b0d53c279acc90"
+}
